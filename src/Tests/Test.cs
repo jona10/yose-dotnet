1fcfad85717497e9db24b8f9f21a22550088cd9f
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2016-09-03 07:59:13 -0400

    Tweak some more to make dotnet core run tests on travis.

---
+++ src/Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void IsZero()
+        {
+            1.Should().Be(0, "because 0 should be 0");
+        }
+    }
+}
