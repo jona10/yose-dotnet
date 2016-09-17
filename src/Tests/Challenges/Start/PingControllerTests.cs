f66c8c25b77b975a795ade4aadcf250ac3d73e40
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2016-09-17 08:59:12 -0400

    The ping controller returns alive.

---
+++ src/Tests/Challenges/Start/PingControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using Tests.Extensions;
+using Xunit;
+using Yose.Controllers;
+
+namespace Tests.Challenges.Start
+{
+    public class PingControllerTests
+    {
+        private PingController _controller;
+        private HttpContext _context;
+
+        public PingControllerTests()
+        {
+            _controller = new PingController();
+            _context = new DefaultHttpContext();
+            _context.Response.Body = new MemoryStream();
+        }
+
+        [Fact]
+        public async Task ContentTypeIsJson()
+        {
+            await _controller.Get(_context);
+
+            _context.Response.ContentType.Should().Be("application/json", "because the returned content should be JSON");
+        }
+
+        [Fact]
+        public async Task ContentIsAlive()
+        {
+            await _controller.Get(_context);
+
+            _context.Response.ReadAsString().Should().Be("{\"alive\":true}", "because the content should show that the server is alive");
+        }
+    }
+}
---
+++ src/Tests/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Tests.Extensions
+{
+    public static class HttpResponseExtensions
+    {
+        public static string ReadAsString(this HttpResponse response)
+        {
+            var reader = new StreamReader(response.Body);
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
